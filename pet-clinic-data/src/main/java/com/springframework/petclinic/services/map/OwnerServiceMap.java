b94f1f9b1db4c673bbc452451c8c31f8587fa3a1
evgeni <gg1786@rambler.ru>
2020-06-13 13:02:05 +0200

    added SpecialtyService and SpecialtyServiceMap

--- pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/OwnerServiceMap.java
@@ -33,25 +33,24 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner save(Owner object) {
 
-        if(object!=null){
-            if(object.getPets()!=null){
+        if (object != null) {
+            if (object.getPets() != null) {
                 object.getPets().forEach(pet -> {
-                    if(pet.getPetType() != null){
-                        if(pet.getPetType().getId()==null){
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
                             pet.setPetType(petTypeService.save(pet.getPetType()));
                         }
-                    }else {
+                    } else {
                         throw new RuntimeException("Pet Type is required");
                     }
-                    if(pet.getId()==null){
+                    if (pet.getId() == null) {
                         Pet savedPet = petService.save(pet);
                         pet.setId(savedPet.getId());
                     }
                 });
             }
-
             return super.save(object);
-        }else {
+        } else {
             return null;
         }
     }
@@ -61,12 +60,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
         super.findById(id);
     }
 
-
     @Override
     public void delete(Owner object) {
         super.delete(object);
     }
-
 
     @Override
     public Owner findByLastName(String lastName) {
