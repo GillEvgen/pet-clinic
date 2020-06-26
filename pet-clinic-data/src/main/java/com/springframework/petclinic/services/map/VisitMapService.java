431c5cc1c71816168158d2ef75ec6d90aca4c893
evgeni <gg1786@rambler.ru>
2020-06-26 13:34:41 +0200

    implemented JPA Visit Service, added loader data

--- pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/VisitMapService.java
@@ -2,9 +2,13 @@ package com.springframework.petclinic.services.map;
 
 import com.springframework.petclinic.model.Visit;
 import com.springframework.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
+@Profile("map")
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
 
@@ -20,10 +24,10 @@ public class VisitMapService extends AbstractMapService<Visit, Long> implements 
 
     @Override
     public Visit save(Visit visit) {
-        if(visit.getPet()==null || visit.getPet().getOwner()==null || visit.getPet().getId()==null
-        || visit.getPet().getOwner().getId()==null){
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
             throw new RuntimeException("Invalid visit");
-            }
+        }
         return super.save(visit);
     }
 
--- pet-clinic-web/src/main/java/com/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.springframework.petclinic.bootstrap;
 
 import com.springframework.petclinic.model.*;
-import com.springframework.petclinic.services.OwnerService;
-import com.springframework.petclinic.services.PetTypeService;
-import com.springframework.petclinic.services.SpecialtyService;
-import com.springframework.petclinic.services.VetService;
+import com.springframework.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialtyService specialtyService) {
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -86,6 +85,12 @@ public class DataLoader implements CommandLineRunner {
         mollyCat.setBirthDate(LocalDate.now());
         owner2.getPets().add(mollyCat);
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(mollyCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+        visitService.save(catVisit);
 
         System.out.println("Loaded owners...");
 
